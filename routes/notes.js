f3084fef926d7adf079c534a5cd454ff7f5d39a9
luccaloopz <luccamartins2012@gmail.com>
2022-04-27 20:54:57 -0700

    wrote the get api route for notes.js. callback function reads the db.json file, parses out the json notes data, and returns the saved notes to client.

--- routes/notes.js
+++ routes/notes.js
@@ -3,11 +3,20 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req,res) => {
-    
+    res.json(`${req.method} request received to get notes`);
+
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.json(JSON.parse(data));
+        };
+    });
 });
 
 notes.post('/', (req,res) => {
-    console.info(`${req.method} request received to add a review`);
+    console.info(`${req.method} request received to add a new note`);
 
     const { title, text } = req.body;
 
@@ -20,7 +29,7 @@ notes.post('/', (req,res) => {
 
         const notesString = JSON.stringify(newNote);
 
-        fs.writeFile(`./db/db.json`, notesString, (err) => { //will this override any existing notes objects that are already present within the db.json file?...i don't think it won't, but make sure
+        fs.writeFile("./db/db.json", notesString, (err) => { //will this override any existing notes objects that are already present within the db.json file?...i don't think it won't, but make sure
             err ? console.err(err) : console.log(`New note for ${notesString.title} has been written to the JSON file`)
         });
 
